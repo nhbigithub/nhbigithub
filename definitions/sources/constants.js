2a86377cbf8280517416121da1e1fa75f245ee65
Jordi EY <jordi.ey.nh@gmail.com>
2022-11-23 08:50:17 +0000

    commit testing

--- definitions/sources/constants.js
+++ definitions/sources/constants.js
@@ -3,4 +3,15 @@ const first_date = "'2018-01-01'";
 module.exports = {
   project_id,
   first_date
-};
+}
+
+function country_group(country){
+  return `
+  case
+    when ${channel} in ('DIR-WEB', 'IND-booking') then 'web'
+    when ${channel} in ('DIR-Expedia') then 'Web'
+    when ${channel} in ('WEB') then ${channel}
+    else 'Other'
+  end`
+  }
+
